33c1ac7f3ba4817c7f52ec8b25c762004043dcbe
Clarissa Roeder <clari1294@gmail.com>
2024-03-11 17:17:15 +0100

    optimising

--- database_persistence.rb
+++ database_persistence.rb
@@ -12,10 +12,6 @@ class DatabasePersistence
     @logger = logger
   end
 
-  def disconnect
-    @db.close
-  end
-
   # will log and execute the SQL statement
   def query(statement, *params)
     @logger.info("#{statement}: #{params}")
@@ -24,25 +20,50 @@ class DatabasePersistence
 
   # fetches a id specified list
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
-    result = query(sql, id)
+    sql = <<~SQL
+      SELECT lists.*,
+      count(todos.id) AS todos_count,
+      count(nullif(todos.completed, true)) AS todos_remaining_count
+      FROM lists
+      LEFT JOIN todos ON todos.list_id = lists.id
+      WHERE lists.id = $1
+      GROUP BY lists.id;
+    SQL
 
-    tuple = result.first
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_list(list_id)
-    {id: list_id, name: tuple["name"], todos: todos }
+    result = query(sql, id)
+    tuple_to_list_hash(result.first)
   end
 
+  # fetches todos for a specified list
+  def find_todos_for_list(list_id)
+    todos_sql = "SELECT * FROM todos WHERE list_id = $1"
+    todos_result = query(todos_sql, list_id)
+
+    todos_result.map do |todo_tuple|
+      { id: todo_tuple["id"],
+        name: todo_tuple["name"],
+        completed: todo_tuple["completed"] == "t" }
+    end
+  end
+
+  # fetches all lists
   def all_lists
-    sql = "SELECT * FROM lists"
+    sql = <<~SQL
+      SELECT lists.*,
+      count(todos.id) AS todos_count,
+      count(nullif(todos.completed, true)) AS todos_remaining_count
+      FROM lists
+      LEFT JOIN todos ON todos.list_id = lists.id
+      GROUP BY lists.id
+      ORDER BY lists.name;
+    SQL
+
     result = query(sql)
 
     # ? A tuple in SQL represents a single instance of a relation, i.e.
     # ? a tuple is a row of data and implemented as a hash in Ruby
     result.map do |tuple|
-      list_id = tuple["id"].to_i
-      todos = find_todos_for_list(list_id)
-      {id: list_id, name: tuple["name"], todos: todos}
+      tuple_to_list_hash(tuple)
     end
   end
 
@@ -83,16 +104,17 @@ class DatabasePersistence
     query(sql, list_id)
   end
 
+  def disconnect
+    @db.close
+  end
+
   private
 
-  def find_todos_for_list(list_id)
-    todos_sql = "SELECT * FROM todos WHERE list_id = $1"
-    todos_result = query(todos_sql, list_id)
-
-    todos_result.map do |todo_tuple|
-      { id: todo_tuple["id"],
-        name: todo_tuple["name"],
-        completed: todo_tuple["completed"] == "t" }
-    end
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i
+    }
   end
 end
--- todo.rb
+++ todo.rb
@@ -23,20 +23,20 @@ end
 # * --- VIEW HELPERS --- * #
 helpers do
   def list_complete?(list)
-    todos_count(list) > 0 && todos_remaining_count(list) == 0
+    list[:todos_count] > 0 && list[:todos_remaining_count] == 0
   end
 
   def list_class(list)
     "complete" if list_complete?(list)
   end
 
-  def todos_count(list)
-    list[:todos].size
-  end
+  # // def todos_count(list)
+  # //   list[:todos].size
+  # // end
 
-  def todos_remaining_count(list)
-    list[:todos].count { |todo| !todo[:completed] }
-  end
+  # // def todos_remaining_count(list)
+  # //   list[:todos].count { |todo| !todo[:completed] }
+  # // end
 
   def sort_lists(lists, &block)
     complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
@@ -123,6 +123,7 @@ end
 get "/lists/:id" do
   @list_id = params[:id].to_i
   @list = load_list(@list_id)
+  @todos = @storage.find_todos_for_list(@list_id)
   erb :list, layout: :layout
 end
 
